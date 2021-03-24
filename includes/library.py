aa7a68e1f59a04c895ad749e9bf58bfba6aad931
greatinfluence <36905103+greatinfluence@users.noreply.github.com>
2021-03-24 18:25:16 +0800

    Add files via upload

--- includes/library.py
+++ includes/library.py
@@ -1,5 +1,6 @@
 from endprog import runtime_error
-from includes.objects import Pair, Empty
+from includes.objects import Pair, Empty, obj
+from config import add_func
 
 def plus(x, *args):
     for y in args:
@@ -55,8 +56,38 @@ def List(*args):
         curlist = Pair(i, curlist)
     return curlist
 
+def define_struct(struct_name, *args, **kwargs):
+    arg = list(args)
+    for key in kwargs:
+        arg.append(key)
+    kwargs["ArGuMents"] = arg
+    kwargs["Unbounded_Arg"] = len(args)
+    kwargs["typename"] = struct_name
+    New_class = type(struct_name, (obj,), kwargs)
+    add_func("make-" + struct_name, lambda *args, **kwargs: New_class(*args, **kwargs))
+    is_struct = lambda x: type(x) is New_class
+    add_func(struct_name + "?", is_struct)
+    for x in arg:
+        def getat():
+            att = x
+            def getatt(y):
+                if not is_struct(y):
+                    runtime_error("Object " + y.__str__() + " is not type " + struct_name + ".")
+                return getattr(y, att)
+            return getatt
+        add_func(struct_name + "-" + x, getat())
+        def setat():
+            att = x
+            def setatt(y, val):
+                if not is_struct(y):
+                    runtime_error("Object " + y.__str__() + " is not type " + struct_name + ".")
+                setattr(y, att, val)
+            return setatt
+        add_func("set-" + struct_name + "-" + x + "!", setat())
+
 func_list = {"+" : plus, "-" : minus, "*" : mult, "/" : divi
         , "quotient" : quotient, "modulo" : modulo
         , "remainder" : modulo, "cons" : cons
         , "list" : List, "=" : numequal, "equal?" : Equal
-        , "string=?" : strequal, "display" : print}
+        , "string=?" : strequal, "display" : print
+        , "define-struct" : define_struct}
